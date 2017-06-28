0d019a0395d76713b873fbaff83fe74916e707a9
tancong123 <ntcongkhtn@gmail.com>
2017-06-28 19:54:29 +0700

    start

---
+++ LibraryManagement/Controllers/User/TrangChuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace LibraryManagement.Controllers.User
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Xem()
+        {
+            dynamic truonghoc= new System.Dynamic.ExpandoObject();
+
+
+            truonghoc.ten = "ffffff";
+            truonghoc.tel = "sssss";
+            truonghoc.key = "value";
+
+
+            
+
+
+
+
+            return View();
+        }
+    }
+}
